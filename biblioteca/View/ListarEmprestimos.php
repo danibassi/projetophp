e17d4bf859b5d63c85de26f21529644c7b860c01
danibassi <49538554+danibassi@users.noreply.github.com>
2019-05-28 23:35:08 -0300

    Update Emprestimo

--- biblioteca/View/ListarEmprestimos.php
+++ biblioteca/View/ListarEmprestimos.php
@@ -15,69 +15,44 @@ $dao = new EmprestimoDAO(new Emprestimo());
 ?>
 <html lang="pt-br">    
     <head>
-        <meta charset="utf-8">
-        <link rel="stylesheet" type="text/css" href="_css/estilo.css">
-        <title>Leitores Cadastrados</title>
-        <style>
-            #stusuarios {
-                font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
-                border-collapse: collapse;
-                width: 100%;
-            }
-            #stusuarios td, #stusuarios th {
-                border: 1px solid #ddd;
-                padding: 8px;
-            }
-            #stusuarios tr:nth-child(even){background-color: #f2f2f2;}
-
-            #stusuarios tr:hover {background-color: #ddd;}
-
-            #stusuarios th {
-                padding-top: 12px;
-                padding-bottom: 12px;
-                text-align: left;
-                background-color: #4CAF50;
-                color: white;
-            }
-        </style>        
+    <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="_css/estilohome.css">
+        <link rel="stylesheet" type="text/css" href="_css/styletableempre.css">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat|Nunito:300&display=swap" rel="stylesheet">
+        <title>Empréstimos</title>
     </head>
     
         <body> 
-            <header>
-                <nav>
-                    <ul class="ul">
-                        <li class="li"><a href="MenuFuncionario.php">Página inicial</a></li>
-                    </ul>
-                </nav>
-            </header>
-        <div style="float: left; width: 20%">
-        <table border=1>
-                <td>
-                    <li><a href="CadastroAutor.php">Cadastrar autor</a></li>
-                    <li><a href="CadastroEditora.php">Cadastrar editora</a></li>
-                    <li><a href="CadastroLivro.php">Cadastrar livro</a></li>
-                    <li><a href="CadastroLeitor.php">Cadastrar Usuário</a></li>
-                    <li><a href="CadastroFuncionario.php">Cadastrar funcionário</a></li>
-                    <li><a href="CadastrarEmprestimo.php">Empréstimos</a></li>
-                    <li><a href="EditarLivro.php">Editar livro cadastrado</a></li>
-                    <li><a href="ListarLeitor.php">Leitores cadastrados</a></li>
-                    <li><a href="ListarLivro.php">Livros cadastrados</a></li>
-                </td>
-            
-            </table>
-                
-        </div>  
-        
-        <div id="divBusca" style="float: left; width: 80%">
-        <table id="stusuarios">
-            <thead>
-                <tr>
-                    <td>Leitor</td>
-                    <td>Livro</td>
-                    <td>Funcionario</td>
-                    <td>Data de Saida</td>
-                    <td>Data Limite</td>
-                    <td>Devolução</td>
+        <div id="caixamenu"> 
+        <div id="menu">
+            <a href="MenuFuncionario.php">Ínicio</a>
+            <a href="CadastroLeitor.php">Leitores</a>
+            <a href="CadastroLivro.php">Livros</a>
+            <a href="Emprestimo.php">Empréstimos</a>
+            <a href="RegistrarDevolucao.php">Devolução</a>
+            <a href="CadastroFuncionario.php">Funcionários</a>
+            <a href="CadastroEditora.php">Editoras</a>
+            <a href="CadastroAutor.php">Autores</a>             
+        </div>
+    </div>  
+
+    <div class="titulo">EMPRÉSTIMOS</div>  
+    <form class="novo" action="Emprestimo.php">
+            <input class="new" type="submit" value="Novo Empréstimo">
+        </form>    
+    <div class="limiter">
+		<div class="container-table100">
+			<div class="wrap-table100">
+				<div class="table100">
+					<table>
+						<thead>
+							<tr class="table100-head">
+                                <th>Leitor</th>
+                                <th>Livro</th>
+                                <th>FuncionÁrio</th>
+                                <th>Data de SaÍda</th>
+                                <th>Data Limite</th>
+                                <th>Devolução</th>
                 </tr>
             </thead>
             <?php foreach($dao->selectEmprestimoPendente() as $key => $value):?>
@@ -93,7 +68,7 @@ $dao = new EmprestimoDAO(new Emprestimo());
                               <input type="hidden" name="idEntrega" value="<?php echo $value->emp_id?>">
                               <input type="hidden" name="livroEntregue" value="<?php echo $value->liv_nome?>">
                               <input type="date" name="dataEntrega" required>
-                              <input type="submit" value="Devolução">
+                              <input class="img" type="image" src="_img/excluir.png"/>
                           </form>
                       </td>
 
@@ -101,11 +76,9 @@ $dao = new EmprestimoDAO(new Emprestimo());
               </tbody>
             <?php endforeach; ?>
         </table>
-        
-        <form action="CadastrarEmprestimo.php">
-            <input type="submit" value="Novo Cadastro">
-        </form>
-        
+        </div>
+        </div>
+        </div>  
         </div>
     </body>
 </html>
